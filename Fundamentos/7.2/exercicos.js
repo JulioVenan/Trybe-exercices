b115ab687c6a6a70ee1997478924c14256829de0
JulioVenan <juliovenancio00@gmail.com>
2022-06-09 15:25:44 -0300

    6.5

--- Fundamentos/7.2/exercicos.js
+++ Fundamentos/7.2/exercicos.js
@@ -1,16 +1,30 @@
-const verifyIsNumber = (value1, value2) => {
-  if (typeof value1 !== 'number' || typeof value2 !== 'number') {
-    throw new Error('Os valores devem ser numéricos');
-  }
+// const verifyIsNumber = (value1, value2) => {
+//   if (typeof value1 !== 'number' || typeof value2 !== 'number') {
+//     throw new Error('Os valores devem ser numéricos');
+//   }
+// };
+
+// const sum = (value1, value2) => {
+//   try {
+//     verifyIsNumber(value1, value2);
+//     return value1 + value2;
+//   } catch (error) {
+//     return error.message;
+//   }
+// };
+
+// console.log(sum(2, '3'));
+
+const customer = {
+  firstName: 'Roberto',
+  age: 22,
+  job: 'Teacher',
 };
 
-const sum = (value1, value2) => {
-  try {
-    verifyIsNumber(value1, value2);
-    return value1 + value2;
-  } catch (error) {
-    return error.message;
-  }
-};
-
-console.log(sum(2, '3'));
+const newKeys = (obj, newKey, value) =>{
+  let key = newKey
+  const val = value
+  obj[key] = val
+  return console.log(obj)
+}
+newKeys(customer, ['Favorite Char on D&d'], 'Barbarian')
