6765d6ea9f6f59677eab8990eb0810864ecce10c
JulioVenan <juliovenancio00@gmail.com>
2022-06-09 15:38:46 -0300

    objects.keys

--- Fundamentos/7.2/exercicos.js
+++ Fundamentos/7.2/exercicos.js
@@ -15,16 +15,44 @@
 
 // console.log(sum(2, '3'));
 
-const customer = {
-  firstName: 'Roberto',
-  age: 22,
-  job: 'Teacher',
+// const customer = {
+//   firstName: 'Roberto',
+//   age: 22,
+//   job: 'Teacher',
+// };
+
+// const newKeys = (obj, newKey, value) =>{
+//   let key = newKey
+//   const val = value
+//   obj[key] = val
+//   return console.log(obj)
+// }
+// newKeys(customer, ['Favorite Char on D&d'], 'Barbarian')
+
+const student1 = {
+  Html: 'Muito Bom',
+  Css: 'Bom',
+  JavaScript: 'Ótimo',
+  SoftSkills: 'Ótimo',
 };
 
-const newKeys = (obj, newKey, value) =>{
-  let key = newKey
-  const val = value
-  obj[key] = val
-  return console.log(obj)
-}
-newKeys(customer, ['Favorite Char on D&d'], 'Barbarian')
+const student2 = {
+  Html: 'Bom',
+  Css: 'Ótimo',
+  JavaScript: 'Omaior',
+  SoftSkills: 'Ótimo',
+  Git: 'Bom', // chave adicionada
+};
+
+const listSkills = (student) => {
+  const arrayOfSkills = Object.keys(student);
+  for (const index in arrayOfSkills) {
+    console.log(`${arrayOfSkills[index]}, Nível: ${student[arrayOfSkills[index]]}`);
+  }
+};
+
+console.log('Estudante 1');
+listSkills(student1);
+
+console.log('Estudante 2');
+listSkills(student2);
