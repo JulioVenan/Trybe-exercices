9acace6d0961c46bfad8455015b5d56acbdebbfd
JulioVenan <juliovenancio00@gmail.com>
2022-06-09 15:01:44 -0300

    exercicios

--- Fundamentos/7.2/exercicos.js
+++ Fundamentos/7.2/exercicos.js
@@ -1,8 +1,16 @@
-const sum = (value1, value2) => {
+const verifyIsNumber = (value1, value2) => {
   if (typeof value1 !== 'number' || typeof value2 !== 'number') {
     throw new Error('Os valores devem ser numéricos');
   }
-  return value1 + value2;
 };
 
-console.log(sum(2, 80));
+const sum = (value1, value2) => {
+  try {
+    verifyIsNumber(value1, value2);
+    return value1 + value2;
+  } catch (error) {
+    return error.message;
+  }
+};
+
+console.log(sum(2, '3'));
