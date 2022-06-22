9d425f451a962dea2ef6f0df4760cc5f898f89e7
JulioVenan <juliovenancio00@gmail.com>
2022-06-22 16:26:04 -0300

    test

--- Fundamentos/8.1/exercicio.js
+++ Fundamentos/8.1/exercicio.js
@@ -3,7 +3,7 @@
 const wakeUp = () => 'Acordando!!';
 const breakfast = () => 'Bora tomar café!!';
 const sleeping = () => 'Partiu dormir!!';
-=======
+
 const doingThings = (func) => func();
 // Ela deve retornar o valor do respectivo parâmetro, neste caso:
 // Acordando!!,
@@ -11,4 +11,4 @@ const doingThings = (func) => func();
 console.log(doingThings(wakeUp));
 console.log(doingThings(breakfast));
 console.log(doingThings(sleeping));
-=======
+
