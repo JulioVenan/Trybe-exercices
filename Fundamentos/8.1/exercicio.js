b87af3900c8dde596a4095a241931986177dc0e5
JulioVenan <juliovenancio00@gmail.com>
2022-06-22 15:26:09 -0300

    exercicio

--- Fundamentos/8.1/exercicio.js
+++ Fundamentos/8.1/exercicio.js
@@ -1,11 +1,11 @@
 // Ao chamar a função doingThings:
-const wakeUp = () => 'Acordando!!'
-const breakfast = () => 'Bora tomar café!!'
-const sleeping = () => 'Partiu dormir!!'
+const wakeUp = () => 'Acordando!!';
+const breakfast = () => 'Bora tomar café!!';
+const sleeping = () => 'Partiu dormir!!';
 const doingThings = (func) => func();
 
 // Ela deve retornar o valor do respectivo parâmetro, neste caso:
 // Acordando!!,
-console.log(doingThings(wakeUp))
-console.log(doingThings(breakfast))
-console.log(doingThings(sleeping))
+console.log(doingThings(wakeUp));
+console.log(doingThings(breakfast));
+console.log(doingThings(sleeping));
